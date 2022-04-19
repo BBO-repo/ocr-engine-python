b2dd815e4f0a5bae1841aa692bbaab03cdf3906c
bbo <bbo>
2022-04-19 06:36:01 +0000

    add helper to generate rotated image dataset

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,14 +1,51 @@
+import os
 import unittest
 from xmlrpc.client import FastMarshaller
 import ocrize.ocrlib
+import cv2
+import numpy as np 
+
+class Helper:
+    @staticmethod
+    def generate_rotated_images(document_path:str) -> None:
+        # load the image
+        img = cv2.imread(document_path)
+        
+        # grab the dimensions of the image and calculate the center of the image
+        (h, w) = img.shape[:2]
+        (cX, cY) = (w // 2, h // 2)
+        
+        name, ext = os.path.splitext(document_path)
+
+        # rotate image for angle [0,360[ degrees around the center of the image
+        for angle in range(360):
+            file_name = name + "_" + str(angle) + "_degree" + ext
+            if os.path.exists(file_name):
+                continue
+            
+            # grab the rotation matrix 
+            M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
+            
+            # compute the new bounding dimensions of the image
+            cos = np.abs(M[0, 0])
+            sin = np.abs(M[0, 1])
+            nW = int((h * sin) + (w * cos))
+            nH = int((h * cos) + (w * sin))
+            
+            # adjust the rotation matrix to take into account translation
+            M[0, 2] += (nW / 2) - cX
+            M[1, 2] += (nH / 2) - cY
+    
+            img_rotated = cv2.warpAffine(img, M, (nW, nH))
+            cv2.imwrite(file_name, img_rotated)
 
 class IntegrationTests(unittest.TestCase):
 
     def test_success_on_dataset(self):
         # first dataset
-        data_directory_1 = "/workspaces/ocr-engine-python/tests/data/2022-04-01-testing-data/insurance-card/"
-        data_directory_2 = "/workspaces/ocr-engine-python/tests/data/2022-04-06-testing-data/insurance-card/"
-        data_directory_3 = "/workspaces/ocr-engine-python/tests/data/2022-04-07-testing-data/insurance-card/"
+        data_directory_1= "/workspaces/ocr-engine-python/tests/data/2022-04-01-testing-data/insurance-card/"
+        data_directory_2= "/workspaces/ocr-engine-python/tests/data/2022-04-06-testing-data/insurance-card/"
+        data_directory_3= "/workspaces/ocr-engine-python/tests/data/2022-04-07-testing-data/insurance-card/"
         
         image_insurance_number = [
             (data_directory_1+"PastedGraphic-1.png", "80756000620054622611"),
@@ -40,6 +77,14 @@ class IntegrationTests(unittest.TestCase):
             status, ocr_result = ocrize.ocrlib.Ocrizer.process(val[0],ocrize.ocrlib.DocType.CARD)
             self.assertTrue(status == ocrize.ocrlib.ProcessingStatus.SUCCESS)
             self.assertTrue(ocr_result == val[1])
+    
+    def test_success_on_rotated_images(self):
+        image_path = "/workspaces/ocr-engine-python/tests/data/2022-04-01-testing-data/insurance-card-rotated/PastedGraphic-2.png"
+        
+        # generate rotated image
+        Helper.generate_rotated_images(image_path)
+        
+        
             
         
 
